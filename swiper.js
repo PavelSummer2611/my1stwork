de248da1b63f417102399137c9b513e2288b3991
Pavel Samar <Spa15@mail.ru>
2024-11-02 05:34:22 +0300

    сделал swiper

--- swiper.js
+++ swiper.js
@@ -1,7 +1,34 @@
-let swiper = new Swiper('brand-slider', {
-pagination: {
-   el: '.swiper-pagination',
-   clickable: true,
+let swiper;
+
+function initSwiper() {
+  // Проверяем, если ширина экрана меньше или равна 320px
+  if (window.matchMedia("(max-width: 320px)").matches) {
+    // Если Swiper еще не инициализирован, создаем его
+    if (!swiper) {
+      swiper = new Swiper('.swiper-container', {
+         
+         centeredSlides: true,
+         loop: false,
+         pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        slidesPerView: 1.3,
+        spaceBetween: 10,
+        centerInsufficientSlides: true,
+      });
+    }
+  } else {
+    // Если ширина больше 320px и Swiper был инициализирован, уничтожаем его
+    if (swiper) {
+      swiper.destroy(true, true);
+      swiper = null;
+    }
+  }
 }
 
-});
+// Запускаем функцию при загрузке страницы
+initSwiper();
+
+// Следим за изменением ширины окна и вызываем функцию
+window.addEventListener('resize', initSwiper);
